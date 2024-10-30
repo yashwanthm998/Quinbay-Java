e43cf2bd515ed442cf33c53e00d7611b89e34b78
Yashwanth M <yashwanthm998@gmail.com>
2024-10-31 00:57:08 +0530

    Program 12

---
+++ HR/Java_String_Reverse.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Java_String_Reverse{
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String rev="";
+        int n=A.length();
+        for(int i=n-1;i>=0;i--)
+        {
+            rev+=A.charAt(i);
+        }
+        if (A.equals(rev))
+        {
+            System.out.println("Yes");
+        }
+        else System.out.println("No");
+    }
+}
+
+
+
