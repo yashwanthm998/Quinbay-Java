eb079d72e586c4cf9f9f4692939c1d7b6ba8717c
Yashwanth M <yashwanthm998@gmail.com>
2024-11-10 20:35:25 +0530

    Advance Concepts

--- HR/Advance/Prime_Checker.java
+++ HR/Advance/Prime_Checker.java
@@ -24,7 +24,7 @@ public class Prime_Checker{
 
 	public static void main(String[] args) {
 		try{
-		BufferedReader br=new BufferedReader(new InputStreamReader(in));
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		int n1=Integer.parseInt(br.readLine());
 		int n2=Integer.parseInt(br.readLine());
 		int n3=Integer.parseInt(br.readLine());
